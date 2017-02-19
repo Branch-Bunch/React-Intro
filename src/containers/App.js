4adf035875800077c00fb725baaa5b95b4cf054d
Jacky Chiu <jacky.chiu888@gmail.com>
2017-02-18 21:35:03 -0500

    Mapped out seach results to object

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,25 @@ import React, { Component } from 'react'
 import Header from '../components/Header'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      users: []
+    }
+  }
+
+  searchRepos(input) {
+    return fetch(`https://api.github.com/search/repositories?q=${input}`)
+      .then(res => res.json())
+      .then(data => data.items.map(repo => ({
+        name: repo.name,
+        author: repo.owner.login,
+        imageURL: repo.owner.avatar_url,
+      })))
+  }
+
   render() {
+    this.searchRepos('react').then(x => console.log(x))
     return (
       <div className="App">
       <Header />
