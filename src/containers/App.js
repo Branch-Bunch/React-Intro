608db6bf8e35120976f6949976e8b7f5a0816849
Jacky Chiu <jacky.chiu888@gmail.com>
2017-02-20 15:18:40 -0500

    Added props to constuctor

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,8 +3,8 @@ import Header from '../components/Header'
 
 class App extends Component {
 
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
       repos: []
     }
