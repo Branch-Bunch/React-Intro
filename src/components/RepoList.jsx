550c71249a8576dc2d8c3ba3f0b382c0805401d8
Jacky Chiu <jacky.chiu888@gmail.com>
2017-02-22 18:47:16 -0500

    New componets

---
+++ src/components/RepoList.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import RepoCell from './RepoCell'
+
+class RepoList extends Component {
+  render() {
+    const { repos } = this.props
+    const repoCells = repos.map(repo => (
+      <RepoCell
+        name={repo.name}
+        author={repo.author}
+        imageURL={repo.imageURL}
+      />
+    ))
+
+    return (
+      <div>
+        {repoCells}
+      </div>
+    )
+  }
+}
+
+export default RepoList
